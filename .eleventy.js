0d7581c1d5f1436dbad87e0082767faaa6181a83
Glitch (almondine-cut-plier) <none>
2020-10-31 14:36:32 +0000

    🌪🐸 Checkpoint ./.eleventy.js:9869298/759 ./src/index.html:9869298/22

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
 // docs: https://www.11ty.io/docs/config/
 
-module.exports = {
-  dir: {
-    includes: "_includes",
-    layouts: "_layouts",
-    input: "src",
-    output: "dist"
-  }
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/css/");
+
+  return {
+    dir: {
+      includes: "_partials",
+      layouts: "_layouts",
+      input: "src",
+      output: "dist"
+    }
+  };
 };
